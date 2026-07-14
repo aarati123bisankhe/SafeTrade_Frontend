2a1eb3be5954ea3c5ec132719942d3673e293024
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-14 23:06:46 +0545

    alert component

--- src/components/common/Alert.tsx
+++ src/components/common/Alert.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 type AlertVariant = "error" | "success" | "info";
 
-type AlertProps = {
+type AlertProps = { //alert component props
   title?: string;
   children: ReactNode;
   variant?: AlertVariant;
