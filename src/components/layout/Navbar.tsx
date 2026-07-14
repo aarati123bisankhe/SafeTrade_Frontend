152e6e7d0ee6fe7c5289062dc8387ab611dbe7c5
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-14 22:41:11 +0545

    add navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 import Button from "../common/Button";
 
-export default function Navbar() {
+export default function Navbar() { //navbar component
   return (
     <header className="navbar">
       <div className="navbar__brand">
