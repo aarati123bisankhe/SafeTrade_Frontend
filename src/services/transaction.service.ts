4ea47ff24b6b7479bbbf88c76a116c492b732868
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-16 01:46:51 +0545

    productService

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -2,7 +2,7 @@ import api from "./api";
 import type { ApiResponse } from "../types/auth.types";
 import type { TradeTransaction } from "../types/transaction.types";
 
-const transactionService = { //todo: rename to transactionService
+const transactionService = {
   async createTransaction(productId: string): Promise<TradeTransaction> {
     const { data } = await api.post<ApiResponse<TradeTransaction>>("/transactions", {
       productId,
